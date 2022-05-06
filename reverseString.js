7e021294708c3706f79f55514eabd762de889571
saisantosh1998 <58393030+saisantosh1998@users.noreply.github.com>
2022-05-06 14:18:34 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,12 +1,14 @@
 // using o(1) approach
-let swap=(arr,i,j)=>{
-    let temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
-}
-var reverseString = function(s) {
-    for(let i=0;i<Math.floor(s.length/2);i++){
-        swap(s,i,s.length-1-i);
+function reverse(s){ 
+   for(let i=0;i<Math.floor(s.length/2);i++){
+        s=swap(s,i,s.length-1-i);
     }
     return s
-};
+}
+let swap=(str,i,j)=>{
+    let sb=str.split('');
+    sb[i]=str[j];
+    sb[j]=str[i];
+    str=sb.join('');
+    return str
+}
